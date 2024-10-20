0d8ce9682ba25681abe748bcb622047e29a85bca
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2024-10-20 10:05:46 -0400

    Working on new app design

--- src/components/Clients/ClientDialog.tsx
+++ src/components/Clients/ClientDialog.tsx
@@ -2,6 +2,7 @@ import { ClientData, EnvironmentOptions } from '@/context/ClientContext';
 import { ProductOptions } from '@/model/ProductOptions';
 import { SelectContent, SelectItem } from '@radix-ui/react-select';
 import { FC, useRef, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import { Button } from '../ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '../ui/dialog';
 import { FormItem, FormLabel } from '../ui/form';
@@ -23,26 +24,21 @@ export const ClientDialog: FC<ClientDialogProps> = ({ isOpen, onOpenChange, clie
   const clientIdRef = useRef<HTMLInputElement>(null);
   const clientSecretRef = useRef<HTMLInputElement>(null);
 
-  const handleSave = () => {
-    if (
-      organizationRef.current &&
-      clientIdRef.current &&
-      clientSecretRef.current &&
-      selectedRegion &&
-      selectedEnvironment
-    ) {
-      const updatedClient: ClientData = {
-        //product: selectedProduct,
-        product: ProductOptions.XMCloud,
-        organizationId: organizationRef.current.value,
-        clientId: clientIdRef.current.value,
-        clientSecret: clientSecretRef.current.value,
-        region: selectedRegion,
-        environment: EnvironmentOptions.Dev,
-        //environment: environmentRef.current.value,
-      };
-      onSave(updatedClient);
-    }
+  const methods = useForm<ClientData>({
+    defaultValues: {
+      product: (client?.product as ProductOptions) || '',
+      organizationId: client?.organizationId || '',
+      clientId: client?.clientId || '',
+      clientSecret: client?.clientSecret || '',
+      region: client?.region || '',
+      environment: (client?.environment as EnvironmentOptions) || '',
+    },
+  });
+
+  const { handleSubmit, setValue } = methods;
+
+  const handleSave = (data: ClientData) => {
+    onSave(data);
   };
 
   const handleProductChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -58,66 +54,72 @@ export const ClientDialog: FC<ClientDialogProps> = ({ isOpen, onOpenChange, clie
             {client ? 'Make changes to the client here.' : 'Add a new client here.'}
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Product</label>
-            <Select onValueChange={setSelectedProduct}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Product" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="XMCloud">XM Cloud</SelectItem>
-                <SelectItem value="XMPlatform">XM Platform</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <FormItem>
-            <FormLabel>Region</FormLabel>
-            <Select onValueChange={setSelectedRegion} defaultValue={client?.region || ''}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Region" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="US">United States</SelectItem>
-                <SelectItem value="EU">Europe</SelectItem>
-                <SelectItem value="AP">Asia Pacific</SelectItem>
-              </SelectContent>
-            </Select>
-          </FormItem>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Organization</label>
-            <Input ref={organizationRef} defaultValue={client?.organizationId || ''} placeholder="Organization" />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Environment</label>
-            <Select defaultValue={client?.environment || ''}>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Environment" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="dev">Development</SelectItem>
-                <SelectItem value="prod">Production</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Client ID</label>
-            <Input ref={clientIdRef} defaultValue={client?.clientId || ''} placeholder="Client ID" />
-          </div>
-          <div>
-            <label className="block text-sm font-medium text-gray-700">Client Secret</label>
-            <Input
-              ref={clientSecretRef}
-              defaultValue={client?.clientSecret || ''}
-              placeholder="Client Secret"
-              type="password"
-            />
-          </div>
-        </div>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(handleSave)} className="grid gap-4 py-4">
+            <FormItem>
+              <FormLabel>Product/Feature</FormLabel>
+              <Select onValueChange={setSelectedProduct}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Product" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.values(ProductOptions).map((product) => (
+                    <SelectItem key={product} value={product}>
+                      {product}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+            <FormItem>
+              <FormLabel>Region</FormLabel>
+              <Select onValueChange={(value) => setValue('region', value)} defaultValue={client?.region || ''}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Region">{methods.watch('region') || 'Region'}</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="US">United States</SelectItem>
+                  <SelectItem value="EU">Europe</SelectItem>
+                  <SelectItem value="AP">Asia Pacific</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+            <FormItem>
+              <FormLabel>Organization</FormLabel>
+              <Input ref={organizationRef} defaultValue={client?.organizationId || ''} placeholder="Organization" />
+            </FormItem>
+            <FormItem>
+              <FormLabel>Environment</FormLabel>
+              <Select defaultValue={client?.environment || ''}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Environment" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.values(EnvironmentOptions).map((env) => (
+                    <SelectItem key={env} value={env}>
+                      {env}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </FormItem>
+            <FormItem>
+              <FormLabel className="block text-sm font-medium text-gray-700">Client ID</FormLabel>
+              <Input ref={clientIdRef} defaultValue={client?.clientId || ''} placeholder="Client ID" />
+            </FormItem>
+            <FormItem>
+              <FormLabel className="block text-sm font-medium text-gray-700">Client Secret</FormLabel>
+              <Input
+                ref={clientSecretRef}
+                defaultValue={client?.clientSecret || ''}
+                placeholder="Client Secret"
+                type="password"
+              />
+            </FormItem>
+          </form>
+        </FormProvider>
         <DialogFooter>
-          <Button type="submit" onClick={handleSave}>
-            Save changes
-          </Button>
+          <Button type="submit">Save changes</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -2,11 +2,10 @@ export const Welcome = () => {
   return (
     <>
       <div className="welcome-box w-full max-w-4xl mb-4 p-10 rounded-lg bg-gray-100">
-        <h1 className="text-xl font-bold mb-4">Welcome to the Sitecore Assistant Chat</h1>
+        <h1 className="text-xl font-bold mb-4">Personalize Assistant</h1>
         <p className="text-md mb-4">
-          This chat is designed to assist you with creating Sitecore assets in the Sitecore SaaS products. Whether you
-          have questions, need guidance, or want to start a conversation, feel free to reach out. Our chat is powered by
-          Vercel AI SDK with OpenAI.
+          This is a Proof of Concept for Sitecore Personalize Assistant slated for Sitecore Marketplace. Goal is to make
+          creating experiences in Sitecore Personalize much easier.
         </p>
         <h2 className="text-lg font-semibold mb-2">Getting Started:</h2>
         <ol className="list-decimal pl-6 mb-4">
