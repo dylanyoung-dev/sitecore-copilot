f906057302f9ffe16ed06c7f4017faa552181a45
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2024-11-18 08:30:35 -0500

    Adjustments

--- src/app/api/chat/route.tsx
+++ src/app/api/chat/route.tsx
@@ -48,3 +48,18 @@ export async function POST(req: NextRequest, res: NextResponse) {
 // Add a tool that can create a new experience in Sitecore Personalize (V2)
 // Add a tool to allow a user to add or create a decision model in Sitecore Personalize (V2) - But only once they have started a flow to create an experience
 // Add a tool to allow them to select from existing recipes (a feature coming soon) in order to create custom recipes (V3)
+
+/*
+Possible Flow of Sitecore Personalize Experience Creation Agent:
+
+1. User asks to create a new experience
+  - This step could be added to determine if they want to use a recipe, create a new experience from scratch or use an existing template
+2. Define Base Experience Details include Name and Assets like Html, CSS, JS and Freemarker API Request
+  - Template Vars could be added Later
+  - Step could be iterative to allow user to make modifications to the experience details to improve the experience
+3. (Optional) Allow user to add or create a decision model
+4. (Optional) Allow user to pick an Audience
+5. Review and Confirm the creation of the Experience
+6. Create the Experience in Sitecore Personalize
+
+*/
